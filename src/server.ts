203d2a3fe2eef54112ed32d762d7b8ecb6c5e39d
Taimoor <taimooribneayub@gmail.com>
2019-12-16 16:04:43 +0500

    Added mongo capabilities

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
 
+import * as mongodb from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-// This is your playground. any change is made to this file will be reflected immediately upon saving. Enjoy.
-console.log('\n\n');
+
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+mongodb.connect(url).then(async (x) => {
+    console.log("DB started");
+
+    // x is the DB Client. we get db from there
+    let db = x.db("simpleDB");
+
+    // from db we get collections
+    let col = db.collection("simple");
+
+    // we add data to collection this way
+    await col.insertOne({ "name": "Jamal" });
+
+    // this is simple query. finds all docs
+    let allDocs = await col.find({}).toArray();
+
+    // & this is just printing
+    console.log(allDocs);
+});
 
 
-var a = 2;
-var b = 4;
-
-var c = a + b;
-
-console.log("This ${}");
